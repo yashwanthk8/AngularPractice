9702a1edf22d69f3121cd7ab15bc4320a0b2b598
YASHWANTH KUMAR <yashwanthkumar@YASHWANTHs-MacBook-Air.local>
2026-07-02 00:12:22 +0530

    feat(combineLatest): fetch latest products as user search

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,8 @@
 import { Routes } from '@angular/router';
+import { ProductFilterComponent } from './components/product-filter/product-filter.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 export const routes: Routes = [
-      
+  { path: 'product-filter', component: ProductFilterComponent },
+  {path: '',component: DashboardComponent}
 ];
